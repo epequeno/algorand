f0533bcc9f0f5b3d5d8a4e6b27dbd28e85e9588e
Steven Pequeno <steven@pequeno.in>
2021-06-12 18:24:22 -0500

    broken but runnable version

--- smart_contracts/simple/simple-rust/src/main.rs
+++ smart_contracts/simple/simple-rust/src/main.rs
@@ -1,9 +1,15 @@
+//!
+//! Implimentation of the "Writing a Simple Smart Contract" tutorial in rust using
+//! manuelmauro/algonaut
+//! https://developer.algorand.org/tutorials/writing-simple-smart-contract/
+//!
 use algonaut_client::algod::v2::Client as AlgodClient;
 use algonaut_client::indexer::v2::message::{Account, QueryAccount};
 use algonaut_client::indexer::v2::Client as IndexerClient;
 use algonaut_client::{Algod, Indexer, Kmd};
-use algonaut_core::{Address, LogicSignature, MicroAlgos};
-use algonaut_transaction::{Pay, SignedTransaction, Txn};
+use algonaut_core::{Address, ApiSignedLogic, MicroAlgos};
+use algonaut_transaction::api_transaction::ApiTransaction;
+use algonaut_transaction::{ApiSignedTransaction, Pay, Txn};
 use data_encoding::BASE64;
 use std::env;
 use std::process::exit;
@@ -81,14 +87,15 @@ impl AlgodEnvironment {
     }
 }
 
-fn get_balance(client: &AlgodClient, address: &str) -> u64 {
+async fn get_balance(client: &AlgodClient, address: &str) -> u64 {
     client
         .account_information(address)
+        .await
         .unwrap()
         .amount_without_pending_rewards
 }
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let algod_config: EnvironmentConfig = AlgodEnvironment::Sandbox.get_config();
     let passphrase = get_val("PASSPHRASE".to_string());
 
@@ -111,10 +118,13 @@ fn main() {
         .unwrap();
 
     // compile teal program
-    let contract = algod_client.compile_teal(TEAL_PROGRAM.into()).unwrap();
+    let contract = algod_client
+        .compile_teal(TEAL_PROGRAM.into())
+        .await
+        .unwrap();
 
     // obtain a handle to our wallet
-    let list_response = kmd.list_wallets().unwrap();
+    let list_response = kmd.list_wallets().await.unwrap();
     let wallet_id = match list_response
         .wallets
         .into_iter()
@@ -123,22 +133,17 @@ fn main() {
         Some(wallet) => wallet.id,
         None => String::new(),
     };
-    let init_response = kmd.init_wallet_handle(&wallet_id, "").unwrap();
+    let init_response = kmd.init_wallet_handle(&wallet_id, "").await.unwrap();
     let wallet_handle_token = init_response.wallet_handle_token;
 
     // for this example, arbitrarily choose the first 2 accounts returned using deafult network
     // config. make sure this way of determining accounts makes sense for the environment.
-    let query: QueryAccount = QueryAccount {
-        application_id: None,
-        asset_id: None,
-        auth_addr: None,
-        currency_greater_than: None,
-        currency_less_than: None,
-        limit: None,
-        next: None,
-        round: None,
-    };
-    let accounts: Vec<Account> = indexer_client.accounts(&query).unwrap().accounts;
+    let accounts: Vec<Account> = indexer_client
+        .accounts(&QueryAccount::default())
+        .await
+        .unwrap()
+        .accounts;
+
     let alice: &Account = &accounts[0];
     let bob: &Account = &accounts[1];
 
@@ -148,11 +153,14 @@ fn main() {
     println!("contract {}\n", contract.hash);
 
     println!("starting balances");
-    println!("{} alice", get_balance(&algod_client, &alice.address));
-    println!("{} bob", get_balance(&algod_client, &bob.address));
-    println!("{} contract\n", get_balance(&algod_client, &contract.hash));
+    println!("{} alice", get_balance(&algod_client, &alice.address).await);
+    println!("{} bob", get_balance(&algod_client, &bob.address).await);
+    println!(
+        "{} contract\n",
+        get_balance(&algod_client, &contract.hash).await
+    );
 
-    let params = algod_client.transaction_params().unwrap();
+    let params = algod_client.transaction_params().await.unwrap();
 
     let t = Txn::new()
         .sender(alice.address.parse().unwrap())
@@ -170,11 +178,15 @@ fn main() {
         .build();
 
     // we need to sign the transaction to prove that we own the sender address
-    let sign_response = kmd.sign_transaction(&wallet_handle_token, "", &t).unwrap();
+    let sign_response = kmd
+        .sign_transaction(&wallet_handle_token, "", &t)
+        .await
+        .unwrap();
 
     // broadcast the transaction to the network
     let send_response = algod_client
         .broadcast_raw_transaction(&sign_response.signed_transaction)
+        .await
         .unwrap();
 
     println!("alice->contract transaction id: {}\n", send_response.tx_id);
@@ -183,6 +195,7 @@ fn main() {
     loop {
         let txn_state = algod_client
             .pending_transaction_with_id(&send_response.tx_id)
+            .await
             .unwrap();
 
         if let Some(_) = txn_state.confirmed_round {
@@ -197,9 +210,12 @@ fn main() {
     }
 
     println!("\nbalances after contract funded");
-    println!("{} alice", get_balance(&algod_client, &alice.address));
-    println!("{} bob", get_balance(&algod_client, &bob.address));
-    println!("{} contract\n", get_balance(&algod_client, &contract.hash));
+    println!("{} alice", get_balance(&algod_client, &alice.address).await);
+    println!("{} bob", get_balance(&algod_client, &bob.address).await);
+    println!(
+        "{} contract\n",
+        get_balance(&algod_client, &contract.hash).await
+    );
 
     // Next step is to provide an argument (password) to the contract account so that it will
     // release its funds to the `close-to` address:
@@ -213,7 +229,7 @@ fn main() {
     println!("closing contract by providing password...");
     let passphrase_arg = passphrase.as_bytes().to_owned();
     let program_bytes = BASE64.decode(contract.result.as_bytes()).unwrap();
-    let lsig = LogicSignature {
+    let lsig = ApiSignedLogic {
         logic: program_bytes,
         sig: None,
         msig: None,
@@ -223,7 +239,7 @@ fn main() {
     let contract_address: Address = contract.hash.parse().unwrap();
     let bob_address: Address = bob.address.parse().unwrap();
 
-    let params = algod_client.transaction_params().unwrap();
+    let params = algod_client.transaction_params().await.unwrap();
 
     let t = Txn::new()
         .sender(contract_address)
@@ -241,23 +257,25 @@ fn main() {
         )
         .build();
 
-    let signed_transaction = SignedTransaction {
+    let signed_transaction = ApiSignedTransaction {
         sig: None,
-        multisig: None,
-        logicsig: Some(lsig),
-        transaction: t,
+        msig: None,
+        lsig: Some(lsig),
+        transaction: ApiTransaction::from(t),
         transaction_id: "".to_owned(),
     };
 
     let transaction_bytes = rmp_serde::to_vec_named(&signed_transaction).unwrap();
     let send_response = algod_client
         .broadcast_raw_transaction(&transaction_bytes)
+        .await
         .unwrap();
 
     // wait for transaction to finalize
     loop {
         let txn_state = algod_client
             .pending_transaction_with_id(&send_response.tx_id)
+            .await
             .unwrap();
 
         if let Some(_) = txn_state.confirmed_round {
@@ -272,7 +290,10 @@ fn main() {
     }
 
     println!("\nbalances after contract closed");
-    println!("{} alice", get_balance(&algod_client, &alice.address));
-    println!("{} bob", get_balance(&algod_client, &bob.address));
-    println!("{} contract", get_balance(&algod_client, &contract.hash));
+    println!("{} alice", get_balance(&algod_client, &alice.address).await);
+    println!("{} bob", get_balance(&algod_client, &bob.address).await);
+    println!(
+        "{} contract",
+        get_balance(&algod_client, &contract.hash).await
+    );
 }
