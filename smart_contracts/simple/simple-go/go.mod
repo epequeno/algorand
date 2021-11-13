ebc6a5db74efcb086e56938fd142d6677b72e890
Steven Pequeno <steven@pequeno.in>
2021-11-12 19:49:06 -0600

    check-in

--- smart_contracts/simple/simple-go/go.mod
+++ smart_contracts/simple/simple-go/go.mod
@@ -3,8 +3,7 @@ module github.com/epequeno/simple-go
 go 1.16
 
 require (
-	github.com/algorand/go-algorand-sdk v1.7.0 // indirect
+	github.com/algorand/go-algorand-sdk v1.11.0
 	github.com/google/go-querystring v1.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
-	golang.org/x/sys v0.0.0-20210507161434-a76c4d0a0096 // indirect
+	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
 )
--- smart_contracts/simple/simple-go/main.go
+++ smart_contracts/simple/simple-go/main.go
@@ -1,32 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
-	"github.com/algorand/go-algorand-sdk/client/algod"
+	"github.com/algorand/go-algorand-sdk/client/v2/algod"
+	"github.com/algorand/go-algorand-sdk/crypto"
+	"github.com/algorand/go-algorand-sdk/future"
 )
 
 func main() {
-	algodAddress := os.Getenv("SANDBOX_ADDRESS")
-	algodToken := os.Getenv("SANDBOX_TOKEN")
+	ctx := context.Background()
+	algodAddress := os.Getenv("SANDBOX_ALGOD_ADDRESS")
+	algodToken := os.Getenv("SANDBOX_ALGOD_TOKEN")
 	if algodAddress == "" || algodToken == "" {
 		fmt.Println("env vars are not set!")
 		return
 	}
 
+	// build clients
 	algodClient, err := algod.MakeClient(algodAddress, algodToken)
 	if err != nil {
 		fmt.Printf("failed to make algod client: %s\n", err)
 		return
 	}
 
-	nodeStatus, err := algodClient.Status()
+	// create contract account
+	dat, err := os.ReadFile("./passphrase.teal")
 	if err != nil {
-		fmt.Println("oops")
-		return
+		panic(err)
 	}
+	compiledTeal, err := algodClient.TealCompile(dat).Do(ctx)
+	if err != nil {
+		panic(err)
+	}
+	lsig := crypto.MakeLogicSigAccountEscrow([]byte(compiledTeal.Result), [][]byte{})
+	sp, err := algodClient.SuggestedParams().Do(ctx)
+	if err != nil {
+		panic(err)
+	}
+	var note []byte
+	future.MakePaymentTxn("", "", 0, note, "", sp)
+	// alice funds escrow
 
-	fmt.Println(nodeStatus.LastRound)
+	// obtain a handle to our wallet and sign txn
+
+	// submit transaction
+
+	// provide password to lsig and submit contract signed transaction
 
 }
